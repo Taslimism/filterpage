5fa2a61b795183a66066367db8cb2d8a7ce12ba3
Taslim Raza Ansari <imtas00@gmail.com>
2022-02-03 20:28:44 +0530

    fixed port error in server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const path = require('path');
 const app = express();
 
