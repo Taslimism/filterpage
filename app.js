57b07f3ff7ae944219723c415ff945499e1fe4c0
Taslim Raza Ansari <imtas00@gmail.com>
2022-02-03 20:52:20 +0530

    built react code

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/', async (req, res) => {
     try {
