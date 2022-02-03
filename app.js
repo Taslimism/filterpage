9808fc9bca19d0ce0f97779dfbd59f290949ecc5
Taslim Raza Ansari <imtas00@gmail.com>
2022-02-03 20:14:34 +0530

    fixed static page serving

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const PORT = 5000 || process.env.PORT;
-
+const path = require('path');
 const app = express();
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', async (req, res) => {
     try {
